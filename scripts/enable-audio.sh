f208ea0c62f9021f54603e852eb0161cb38831fc
Bob Jarvis <bob@zeal.co>
2024-04-10 14:18:07 +0930

    shhhh

--- scripts/enable-audio.sh
+++ scripts/enable-audio.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # unmute audio on hifiberry miniamp board
-sudo -i
-echo "16" > /sys/class/gpio/export 
-echo "out" > /sys/class/gpio/gpio16/direction
-echo "1" > /sys/class/gpio/gpio16/value 
-echo "16" > /sys/class/gpio/unexport
-exit
+sudo echo "16" > /sys/class/gpio/export 
+sudo echo "out" > /sys/class/gpio/gpio16/direction
+sudo echo "1" > /sys/class/gpio/gpio16/value 
+sudo echo "16" > /sys/class/gpio/unexport
